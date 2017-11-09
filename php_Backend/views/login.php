b0277e306cca1e8da07005ebaf55f2c553df6961
Susanne Schaefer <s0553405@htw-berlin.de>
2017-11-09 10:41:34 +0100

    Frontend Login

--- php_Backend/views/login.php
+++ php_Backend/views/login.php
@@ -1,27 +1,39 @@
 <html>
 <head>
+    <link href="myStylesLogin.css" rel="stylesheet"></link>
 <title>Login</title>
 </head>
   <body>
+    <header>
+        <p>Sprache deutsch englisch</p>
+    </header>
     <?php
-      echo $result;
+    echo $result;
       ?>
+    <img src="01_WSG_logo.png" alt="Logo" style="width: 400px">
+
       <form action=" " method="POST">
-        <p>
-          <label> Username</label>
-          <input id="username" value=" " name="username" type="text" required="required" />
-          <br />
+          <p class="logintext"><b>LOGIN</b></p>
+          <p class="abstand" >
+          <input id="username"  name="username"  placeholder="Kundennummer" type="text" required="required" />
+
         </p>
-        <p>
-          <label>Passwort</label>
-          <input id="password" name="password" type="password" required="required"  />
+        <p class="abstand">
+          <input id="password" name="password" placeholder="Passwort" type="password" required="required"  />
         </p>
-        <br />
-        <p>
-          <button type="submit"><span>Login</span></button>
+
+        <p class="abstandnachButton">
+          <button type="submit" class="abstandButton"><span>Einloggen</span></button>
           <button type="reset"><span>Abbrechen</span></button>
         </p>
       </form>
+
+  <footer>
+      <a href="https://waggonservice.com/impressum.html" target="_blank">Impressum</a>
+      <a href="https://waggonservice.com/#contact" target="_blank">Kontakt</a>
+      <a href="https://waggonservice.com/" target="_blank">waggonservice.com</a>
+  </footer>
+
   </body>
 
 </html>
