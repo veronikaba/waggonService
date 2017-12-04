aafbeb4ddb2d583539b979e292e87dbf321cd568
Susanne Schaefer <s0553405@htw-berlin.de>
2017-12-04 11:13:16 +0100

    Suchfunktion

--- php_Backend/views/afterlogin.php
+++ php_Backend/views/afterlogin.php
@@ -43,11 +43,11 @@
 
  <div class="inner-addon right-addon">
      <i class="glyphicon glyphicon-search"></i>
-     <input type="text" class="form-control" placeholder="Suche" />
+     <input class="form-control"  type="text" id= "myInput" onkeyup= "myFunktion()"  placeholder="Suche" />
  </div>
 
  <div style="overflow-x:auto;">
-    <table class="table  table-striped table-hover">
+     <table id="myTable">
         <thead>
         <tr>
             <th>Auftragsnummer</th>
@@ -72,6 +72,35 @@
         </tbody>
 
     </table>
+     <script>
+         function myFunktion() {
+             // Declare variables
+             var input, filter, table, tr, td, i;
+             input = document.getElementById("myInput");
+             filter = input.value.toUpperCase();
+             table = document.getElementById("myTable");
+             tr = table.getElementsByTagName("tr");
+
+             // Loop through all table rows, and hide those who don't match the search query
+             for (i = 0; i < tr.length; i++) {
+                 if (!tr[i].classList.contains('header')) {
+                     td = tr[i].getElementsByTagName("td"),
+                         match = false;
+                     for (j = 0; j < td.length; j++) {
+                         if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                             match = true;
+                             break;
+                         }
+                     }
+                     if (!match) {
+                         tr[i].style.display = "none";
+                     } else {
+                         tr[i].style.display = "";
+                     }
+                 }
+             }
+         }
+     </script>
  </div>
      <div class="push"></div>
  </div>
