295aed1dc8bcd532ea6d560a321701c2069c79ad
Susanne Schaefer <s0553405@htw-berlin.de>
2017-12-06 21:06:42 +0100

    login Fehler behoben

--- php_Backend/views/login.php
+++ php_Backend/views/login.php
@@ -15,7 +15,7 @@
 
     <div class="col-12">
         <img src="views/02_WSG_logo.png" alt="Logo" class="logo"">
-        <form action="afterlogin.php "  method="POST">
+        <form action="views/afterlogin.php "  method="POST">
             <p class="logintext"><b>LOGIN</b></p>
             <input id="username" class="form-control" style="width:180px;" name="username"  placeholder="Kundennummer" type="text" required="required" />
             <input id="password" class="form-control" style="width:180px;" name="password" placeholder="Passwort" type="password" required="required"  />
