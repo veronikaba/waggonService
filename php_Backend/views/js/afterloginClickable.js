5af479499c40832172ed017449dff58bbd27d207
root <root@debian.f4.htw-berlin.de>
2017-12-14 11:22:06 +0100

    changed afterloginClickable.js

--- php_Backend/views/js/afterloginClickable.js
+++ php_Backend/views/js/afterloginClickable.js
@@ -1,7 +1,7 @@
 src="http://code.jquery.com/jquery-latest.pack.js";
 
     $(document).ready(function() {
-        $('tr#row').click(function(event) {
+        $('.table > tbody > tr').click(function(event) {
             window.location.href = 'http://waggonservice.f4.htw-berlin.de/views/orderdetail.php';
         });
     });
