48bff566a78096d9af9baa651392d9ba0f42c354
Veronika Babic <s0554747@htw-berin.de>
2017-12-14 11:50:32 +0100

    trying to make row clickable

--- php_Backend/views/js/afterloginClickable.js
+++ php_Backend/views/js/afterloginClickable.js
@@ -1,7 +1,13 @@
 src="http://code.jquery.com/jquery-latest.pack.js";
 
-    $(document).ready(function() {
-        $('.table > tbody > tr').click(function(event) {
-            window.location.href = 'http://waggonservice.f4.htw-berlin.de/views/orderdetail.php';
-        });
+$(document).ready(function() {
+
+    $('#row tr').click(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            window.location = 'http://waggonservice.f4.htw-berlin.de/views/orderdetail.php';
+        }
     });
+
+});
+
