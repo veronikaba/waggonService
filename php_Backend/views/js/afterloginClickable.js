c4de1e69af57ee4ee8199d3eb283633d47578567
Mara Broeker <marabroker@yahoo.de>
2018-01-08 23:55:20 +0100

    MVC Logik einarbeiten

--- php_Backend/views/js/afterloginClickable.js
+++ php_Backend/views/js/afterloginClickable.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     $(".clickable-row").click(function() {
-        window.location = $(this).data("href");
+        window.location = $(this).data("href" + "?id=" + $(this).getElementByTagName("td jobnumber").innerHTML);
     });
 });
 
