29ce62faa26e4cda4fdc2c341ea39843d99d3bc8
Mara Broeker <marabroker@yahoo.de>
2018-01-09 00:18:06 +0100

    MVC Logik einarbeiten

--- php_Backend/views/orderdetail.php
+++ php_Backend/views/orderdetail.php
@@ -74,8 +74,7 @@ include(ABS_PATH . '/models/modelDatabase.php');
 
     <div class="rows grey numberheader">
         <div class="col-3">Auftragsnummer</div>
-        <div class="col-9"><?=
-        ?>
+        <div class="col-9"> BENIS
 </div>
     </div>
 
