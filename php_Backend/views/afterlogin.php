28afaf51acb84852c5d9b3c9ce2b17ae4818394a
Susanne Schaefer <s0553405@htw-berlin.de>
2018-01-09 14:52:06 +0100

    Auslagerung Suchfunktion

--- php_Backend/views/afterlogin.php
+++ php_Backend/views/afterlogin.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="views/css/myStylesAfterLogin.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="views/js/afterloginClickable.js"></script>
+    <script src="views/js/afterloginSearchFunction.js"></script>
 
 
 </head>
@@ -97,35 +98,6 @@ if(mysqli_connect_errno($db)) {
 
 
         </table>
-        <script>
-            function myFunktion() {
-                // Declare variables
-                var input, filter, table, tr, td, i;
-                input = document.getElementById("myInput");
-                filter = input.value.toUpperCase();
-                table = document.getElementById("myTable");
-                tr = table.getElementsByTagName("tr");
-
-                // Loop through all table rows, and hide those who don't match the search query
-                for (i = 0; i < tr.length; i++) {
-                    if (!tr[i].classList.contains('header')) {
-                        td = tr[i].getElementsByTagName("td"),
-                            match = false;
-                        for (j = 0; j < td.length; j++) {
-                            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-                                match = true;
-                                break;
-                            }
-                        }
-                        if (!match) {
-                            tr[i].style.display = "none";
-                        } else {
-                            tr[i].style.display = "";
-                        }
-                    }
-                }
-            }
-        </script>
     </div>
     <div class="pushFooter"></div>
 </div>
